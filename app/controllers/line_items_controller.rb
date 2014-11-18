7f6218556ad50b655b1a5f4ee0de8df2aee9aad7
Denis Smolov <densmolov@gmail.com>
2014-11-19 01:00:04 +0300

    20_Line_items are removed 1-by-1

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -57,9 +57,13 @@ class LineItemsController < ApplicationController
   # DELETE /line_items/1
   # DELETE /line_items/1.json
   def destroy
-    @line_item.destroy
+    @line_item = LineItem.find(params[:id])
+    @cart = Cart.find_by(id: @line_item.cart_id)
+    @line_item = @cart.remove_product(@line_item.id)
+    if @line_item.quantity == 0
+      @line_item.destroy
+    end
     respond_to do |format|
-      #format.html { redirect_to line_items_url, notice: 'Line item was successfully destroyed.' }
       format.html { redirect_to carts_url + '/' + session[:cart_id].to_s,
         notice: 'Line item was successfully removed from your cart.' }
       format.json { head :no_content }
@@ -67,13 +71,14 @@ class LineItemsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_line_item
-      @line_item = LineItem.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_line_item
+    @line_item = LineItem.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def line_item_params
-      params.require(:line_item).permit(:product_id)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def line_item_params
+    params.require(:line_item).permit(:product_id)
+  end
+
 end
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,19 +1,26 @@
 class Cart < ActiveRecord::Base
-	has_many :line_items, dependent: :destroy
+  has_many :line_items, dependent: :destroy
 
-	def add_product(product_id)
-		current_item = line_items.find_by(product_id: product_id)
-		if current_item
-			current_item.quantity += 1
-		else
-			current_item = line_items.build(product_id: product_id)
-		    current_item.price = current_item.product.price
-		end
-		return current_item
-	end
+  def add_product(product_id)
+    current_item = line_items.find_by(product_id: product_id)
+    if current_item
+      current_item.quantity += 1
+    else
+      current_item = line_items.build(product_id: product_id)
+      current_item.price = current_item.product.price
+    end
+    return current_item
+  end
 
-	def total_price
-		line_items.to_a.sum { |item| item.total_price }
-	end
+  def remove_product(line_item_id)
+    current_item = line_items.find_by(id: line_item_id)
+    current_item.quantity -= 1
+    current_item.save!
+    return current_item
+  end
+
+  def total_price
+    line_items.to_a.sum { |item| item.total_price }
+  end
 
 end
