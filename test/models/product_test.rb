831ee5a42d2aa453ad22df69f28984ecfd41431f
densmolov <densmolov@gmail.com>
2014-11-18 17:27:38 +0300

    18_Add products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,86 +1,63 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-	fixtures :products
-	
-#________________________________________________________________
+  fixtures :products
 
-	test "product attributes must not be empty" do
-		product = Product.new
-		assert product.invalid?
-		assert product.errors[:title].any?
-		assert product.errors[:description].any?
-		assert product.errors[:price].any?
-		assert product.errors[:image_url].any?
-	end
+  test "product attributes must not be empty" do
+  	product = Product.new
+	assert product.invalid?
+	assert product.errors[:title].any?
+	assert product.errors[:description].any?
+	assert product.errors[:price].any?
+	assert product.errors[:image_url].any?
+  end
 
-#________________________________________________________________
+  test "product price must be positive" do
+	product = Product.new(title: "My book Title", description: "yyy", image_url: "zzz.jpg")
+	product.price = -1
+	assert product.invalid?
+	assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+	product.price = 0
+	assert product.invalid?
+	assert_equal ["must be greater than or equal to 0.01"], product.errors[:price]
+	product.price = 1
+	assert product.valid?
+  end
 
-	test "product price must be positive" do
-		product = Product.new(title: "My book Title", description: "yyy",
-			image_url: "zzz.jpg")
 
-		product.price = -1
-		assert product.invalid?
-		assert_equal ["must be greater than or equal to 0.01"],
-			product.errors[:price]
-
-		product.price = 0
-		assert product.invalid?
-		assert_equal ["must be greater than or equal to 0.01"],
-			product.errors[:price]
-
-		product.price = 1
-		assert product.valid?
-	end
-
-#________________________________________________________________
-
-	def new_product(image_url)
-		Product.new(title: "My Book Title",
+  def new_product(image_url)
+	Product.new(title: "My Book Title",
 			description: "yyy",
 			price: 1,
 			image_url: image_url)
+  end
+
+  test "image_url" do
+	ok = %w{ fred.gif fred.jpg fred.png FRED.jpg FRED.jpg http://a.b.c/x/y/z/fred.gif }
+	bad = %w{ fred.doc fred.gif/more fred.gif.more }
+	ok.each do |name|
+	  assert new_product(name).valid?, "#{name} shouldn't be invalid"
 	end
-
-	test "image_url" do
-		ok = %w{ fred.gif fred.jpg fred.png FRED.jpg FRED.jpg
-			http://a.b.c/x/y/z/fred.gif }
-		bad = %w{ fred.doc fred.gif/more fred.gif.more }
-
-		ok.each do |name|
-			assert new_product(name).valid?, "#{name} shouldn't be invalid"
-		end
-
-		bad.each do |name|
-			assert new_product(name).invalid?, "#{name} shouldn't be valid"
-		end
+	bad.each do |name|
+      assert new_product(name).invalid?, "#{name} shouldn't be valid"
 	end
+  end
 
-#________________________________________________________________
-
-    test "product is not valid without a unique title" do
-        product = Product.new(title: products(:ruby).title, description: "yyy",
+  test "product is not valid without a unique title" do
+  	product = Product.new(title: products(:ruby).title, description: "yyy",
 			price: 1, image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+  end
 
-        assert product.invalid?
-
-        assert_equal ["has already been taken"], product.errors[:title]
-    end
-
-#________________________________________________________________
-
-	test "product title length must be greater than or equal to 10 symbols" do
-		product = Product.new(description: "yyy", image_url: "zzz.jpg", price: 1)
-
-		product.title = "too short"    # 9 symbols
-		assert product.invalid?
-		assert_equal ["is too short (minimum is 10 characters)"],
-			product.errors[:title]
-		assert product.errors[:title].any?
-
-		product.title = "It is long enough"    # 17 symbols
-		assert product.valid?
-	end
+  test "product title length must be greater than or equal to 10 symbols" do
+	product = Product.new(description: "yyy", image_url: "zzz.jpg", price: 1)
+	product.title = "too short"    # 9 symbols
+	assert product.invalid?
+	assert_equal ["is too short (minimum is 10 characters)"], product.errors[:title]
+	assert product.errors[:title].any?
+    product.title = "It is long enough"    # 17 symbols
+    assert product.valid?
+  end
 
 end
