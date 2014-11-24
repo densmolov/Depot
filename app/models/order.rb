163aca9abf99a15b54a80d614e2854d143818cb6
Denis Smolov <densmolov@gmail.com>
2014-11-24 03:52:00 +0300

    34_Payment_types

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,12 @@
 class Order < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
-  PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
+  #PAYMENT_TYPES = [ "Check", "Credit card", "Purchase order"]
+  belongs_to :pay_type
+  #validates_presence_of :pay_type_id
 
   validates :name, :address, :email, presence: true
-  validates :pay_type, inclusion: PAYMENT_TYPES
+  #validates :pay_type, inclusion: PAYMENT_TYPES
 
   def add_line_items_from_cart(cart)
     cart.line_items.each do |item|
