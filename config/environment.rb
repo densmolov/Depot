eab2b1a70be4e86d8e7c9bd5203ace17691f2202
densmolov <densmolov@gmail.com>
2014-11-24 18:52:41 +0300

    37_We can send mails (with tests OK)

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,17 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# Sending emails
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :smtp #OR :sendmail or :test
+  config.action_mailer.smtp_settings = {
+    address:               "smtp.gmail.com",
+    port:                  587,
+    domain:                "domain.of.sender.net",
+    authentication:        "plain",
+    user_name:             "user_name",
+    password:              "password",
+    enable_starttls_auto:  true
+  }
+end
