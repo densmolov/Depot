50bff3d2178e4ce01ddcde37b7730b5706e04320
Denis Smolov <densmolov@gmail.com>
2014-11-29 00:22:03 +0300

    40_User and admin

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Login and/or password are incorrect."
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to store_url, notice: "You've just signed out."
+  end
+end
