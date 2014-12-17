e2d87b68027aab0dd743002f874224bc3a7d6b10
Denis Smolov <densmolov@gmail.com>
2014-12-18 02:17:48 +0300

    More style and functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,16 @@ class Product < ActiveRecord::Base
 
   before_destroy :ensure_not_referenced_by_any_line_item
 
-  validates :title, :description, :image_url, presence: true
+  validates :title, #:author,
+    :description, :image_url, presence: true
   validates :price, numericality: {greater_than_or_equal_to: 0.01, less_than_or_equal_to: 1000.00}
   validates :title, uniqueness: true
   validates :image_url, allow_blank: true, format: {
-    with:    %r{\.(gif|jpg|png)\Z}i,
+    with:    %r{\.(gif|jpg|jpeg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
   validates :title, length: {minimum: 10}
+  #validates :author, length: {minimum: 3}
 
   def self.latest
     Product.order(:updated_at).last
